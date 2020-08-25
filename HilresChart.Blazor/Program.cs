a1bd1838dc6dc144026b744e0115f32b170f9606
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2020-08-25 11:58:56 +0900

    Added Serilog

--- HilresChart.Blazor/Program.cs
+++ HilresChart.Blazor/Program.cs
@@ -4,8 +4,11 @@
 
 namespace HilresChart.Blazor
 {
+    using System;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
 
     /// <summary>
     /// Main program class.
@@ -16,9 +19,28 @@ namespace HilresChart.Blazor
         /// Start of main program.
         /// </summary>
         /// <param name="args">Command line arguments.</param>
-        public static void Main(string[] args)
+        /// <returns>1 if error. Otherwise good.</returns>
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            SetupSerilog();
+            try
+            {
+                Log.Information(" -=-=-=-=-=- Application START -=-=-=-=-=-");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception e)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                Log.Fatal("Fatal Error: {0}", e.Message);
+                return 1;
+            }
+            finally
+            {
+                Log.Information(" -=-=-=-=-=- Application END -=-=-=-=-=-");
+                Log.CloseAndFlush();
+            }
         }
 
         /// <summary>
@@ -28,9 +50,24 @@ namespace HilresChart.Blazor
         /// <returns>IHostBuilder.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /// <summary>
+        /// Setup Serilog.
+        /// </summary>
+        private static void SetupSerilog()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
     }
 }
